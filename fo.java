23bfc68cbde89c092c92f9e5b6b61da3862699a6
Yuheswari <yuheswari2525@gmail.com>
2025-07-23 07:16:00 +0530

    simple interst

--- fo.java
+++ fo.java
@@ -1,11 +1,17 @@
 package placement;
-import java.util. Scanner;
+import java.util.Scanner;
 public class fo{
-    Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
-    for(int i=0;i<=n;i++){
-        for(j=0;j<=n;j++){
-            
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 || i == n-1 || j == 0 || j == n-1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
         }
     }
 }
