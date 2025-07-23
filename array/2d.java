4fa6c8e23d2c4d0915c43ac0c7c812fd4daa4899
Yuheswari <yuheswari2525@gmail.com>
2025-07-23 19:39:19 +0530

    java

---
+++ array/2d.java
@@ -0,0 +1,15 @@
+package placement;
+import java.util.Scanner;
+import java.util.Arrays;
+public class 2d{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int a[][]=new int[tr][tc];
+        for(int i=0;i<tr;i++){
+            for(int j=0;j<tc;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+    }
+}
