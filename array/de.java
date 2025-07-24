562164f3eecf655a67ff5f3838cf2c81978667da
Yuheswari <yuheswari2525@gmail.com>
2025-07-24 11:49:15 +0530

    arr

---
+++ array/de.java
@@ -0,0 +1,21 @@
+package placement;
+import util.java.*;
+public class de{
+public static void main(String[]args){
+    Scanner sc =new Scanner(System.in);
+    System.out.println("Enter the size of an array");
+    int size =sc.nextInt();
+    //decalaration of array
+    int arr[]=new int[size];
+    for(i=0;i<size;i++){
+        a[i]=sc.nextIn();
+    }
+    System.out.println("enter"+size);
+    for(i=0;i<size;i++){
+
+    }
+    System.out.println(a[i]);
+
+
+}
+}
