ec9954ce06c82c5e918544768988ea935d4e063b
Yuheswari <yuheswari2525@gmail.com>
2025-07-30 22:09:53 +0530

    reversed

---
+++ strings/reverse.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.regex.*;
+class reverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String uk = "ukie";
+        for(int i=0;i<uk.length();i++){
+            System.out.print(uk.charAt(i));
+        }
+    }}
