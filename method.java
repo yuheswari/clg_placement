40eb6a80e4089ec023c391baf00975322282b2e3
Yuheswari <yuheswari2525@gmail.com>
2025-07-30 21:49:56 +0530

    reversed

---
+++ method.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.regex.*;
+class method {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String uk ="yuheswari";
+        String reverse=" ";
+        for(int i = uk.length()-1; i>=0; i--){
+            reverse =reverse+uk.charAt(i);
+        }
+        System.out.println("The reversed number is:"+reverse);
+
+        
+    }}
