151e55643eb40aa39c659a82555c21c4f9134728
Yuheswari <yuheswari2525@gmail.com>
2025-07-31 12:13:12 +0530

    const

--- oops/Const.java
+++ oops/Const.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 public class Const{
     //step1 :construtor name is same as class name
+    Const(String n ){ //parameterised constructor
+        System.out.println(n+"hii");
+    }
+
     Const(){
-        System.out.println("hii");
+        System.out.println("Have the best day!!");
     }
     String name;
     void login(String name){
@@ -11,8 +15,11 @@ public class Const{
     }
     public static void main(String[]args){
         Scanner sc =new Scanner(System.in);
-        Const c =new Const();
+        Const c =new Const("yuheswari");
         c.login("yuhes");
+        Const c1 =new Const();
+
+
     }
 
 }
