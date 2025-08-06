c9efa88d75503e5c36e12cb5c88f3a67b2c470f5
Yuheswari <yuheswari2525@gmail.com>
2025-08-06 09:01:54 +0530

    exceptions

---
+++ exception/demo.java
@@ -0,0 +1,11 @@
+class demo{
+    public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+        try{
+            int n =sc.nextInt();
+        }
+        catch(Exception e){
+            
+        }
+    }
+}
