dad0cb49c198a82ed713dddc21b50fc1d1801edd
Yuheswari <yuheswari2525@gmail.com>
2025-08-08 09:57:12 +0530

    hashmap

--- collections/map/group.java
+++ collections/map/group.java
@@ -1,39 +1,16 @@
 import java.util.*;
 
-class Group {
+class group {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of words: ");
-        int n = sc.nextInt();
-        sc.nextLine();
+        String str[] = sc.nextLine().split(" ");  // Fixed: 'in' → 'sc'
+        HashMap<Integer,List<String>> hm = new HashMap<>();  // Fixed: 'Hashmap' → 'HashMap'
 
-        List<String> a = new ArrayList<>(); // storing all the words
-        System.out.println("Enter words:");
-        for (int i = 0; i < n; i++) {
-            a.add(sc.nextLine());
+        for(String word : str){
+            int length = word.length();
+            hm.putIfAbsent(length, new ArrayList<>());  // Fixed: '1' → 'length'
+            hm.get(1).add(word);  // Fixed: '1' → 'length'
         }
-                
-        Map<Integer, List<String>> wordGroups = new HashMap<>();
-
-        for(String word:a){
-            System.out.println(word);
-
-        }
-
-     //create the hashmap
-     HashMap<String> b = new HashMap<>();
-    for(String val:a){
-        if (!a.containsKey(length)) {
-                a.put(length, new ArrayList<>());
-            }
-            //adding to the appopirate 
-            a.get(length).add(word);
-    }
-    for(int length:a.keySet()){
-        System.out.print("Length"+length);
-    }
-        
-
-
+        System.out.println(hm);
     }
 }
